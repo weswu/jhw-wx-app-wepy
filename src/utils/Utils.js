b8732b5789b5b7c61f52f872dcbb259a063009e8
weswu <836790625@qq.com>
2018-01-26 17:07:10 +0800

    wxw

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -71,9 +71,37 @@ const price = (v) => {
   f = Math.round(v * 100) / 100
   return f
 }
+/*
+ * @author: wes
+ * @date: 2017-7-28
+ * @desc: 时间格式化
+*/
+function formatTime(date, format) {
+  date = new Date(date)
+  format = format || 'yyyy-MM-dd hh:mm:ss'
+  var o = {
+    'M+': date.getMonth() + 1, // 月份
+    'd+': date.getDate(), // 日
+    'h+': date.getHours(), // 小时
+    'm+': date.getMinutes(), // 分
+    's+': date.getSeconds(), // 秒
+    'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+    'S': date.getMilliseconds() // 毫秒
+  }
+  if (/(y+)/.test(format)) {
+    format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(format)) {
+      format = format.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+    }
+  }
+  return format
+}
 
 module.exports = {
   del,
   scrollList,
-  price
+  price,
+  formatTime
 };
