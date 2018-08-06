59237ab5ba0435d5f386f422d58f5e32debca329
weswu <836790625@qq.com>
2018-08-06 21:18:25 +0800

    wxw

--- src/api/api.js
+++ src/api/api.js
@@ -19,6 +19,8 @@ const customData = (params) => http(params, host + 'custom/detail')
 const customUpdate = (params) => http(params, host + 'custom/update')
 // 地区
 const area = (params) => http(params, host + 'area/list')
+// 配置
+const webinfo = (params) => http(params, host + 'webinfo/detail/' + params.id)
 // 账号安全
 const accountInfo = (params) => http(params, host + 'user/accountInfo/' + params.id)
 const accountOauth = (params) => http(params, host + 'oauth/list')
@@ -38,7 +40,6 @@ const messageDetail = (params) => http(params, host + 'message/detail' + (params
 const messageBind = (params) => http(params, host + 'message/bind/detail' + (params.data.enterpriseId ? '/' + params.data.enterpriseId : ''))
 // 设置
 const password = (params) => http(params, host + 'user/detail/password')
-const log = (params) => http(params, host + 'log/list')
 const serviceFeedback = (params) => http(params, 'https://api.jihui88.net/crm_api/index.php/jihui/jihuifeedback')
 // 相册
 const album = (params) => http(params, host + 'album/attr/list/' + params.id)
@@ -67,10 +68,6 @@ const bindDetail = (params) => http(params, host + 'bind/detail' + (params.id ? 
 const member = (params) => http(params, host + 'member/list')
 const memberDetail = (params) => http(params, host + 'member/detail' + (params.id ? '/' + params.id : ''))
 const memberRank = (params) => http(params, host + 'member/rank/list')
-// 分销
-const dMember = (params) => http(params, wxHost + '/wechat/cps/index.php/jihui_api/members/' + params.id + '/' + params.page + '/5' + (params.name ? '/' + params.name : ''))
-const dData = (params) => http(params, wxHost + '/wechat/cps/index.php/jihui_api/ranking/' + params.id + '/' + params.index)
-const dBouns = (params) => http(params, wxHost + '/wechat/cps/index.php/jihui_api/bouns/' + params.id + '/' + params.page + '/4')
 // 订单
 const order = (params) => http(params, host + 'order/list')
 const orderReview = (params) => http(params, host + 'productreview/list')
@@ -81,9 +78,6 @@ const orderDetail = (params) => http(params, host + 'order/detail/' + params.id)
 const orderExpress = (params) => http(params, host + 'order/express')
 const orderPrice = (params) => http(params, host + 'order/priceDetail')
 const orderUpdate = (params) => http(params, host + 'order/update')
-// 商城数据
-const data = (params) => http(params, host + 'order/data')
-const purchase = (params) => http(params, host + 'order/data/member_list')
 // 服务缴费
 const receiptList = (params) => http(params, host + 'crm/receipt/list')
 const receiptLog = (params) => http(params, host + 'crm/receipt/log')
@@ -102,18 +96,12 @@ const signIn = (params) => http(params, host + 'integral_rule/daily')
 const spread = (params) => http(params, host + 'poster/list?pageSize=72')
 const spreadRank = (params) => http(params, wxHost + '/rest/api/comm/poster/userlist')
 const posters = (params) => http(params, 'https://api.jihui88.net/qrcode_poster/api/poster')
-// 微传单
-const wcd = (params) => http(params, host + 'wcd/list')
-const wcdCase = (params) => http(params, host + 'wcd/case')
-// 微网站
-const mobile = (params) => http(params, host + 'mobile/list')
-const mobileCase = (params) => http(params, host + 'mobile/case')
 
 module.exports = {
   wxapplogin,
   // 基本数据
   user, userInfo, enterprise, customData, customUpdate,
-  area,
+  area, webinfo,
   // 账号安全
   accountInfo, accountOauth,
   login, logout,
@@ -125,7 +113,6 @@ module.exports = {
   message, messageDetail, messageBind,
   // 设置
   password,
-  log,
   serviceFeedback,
   // 相册
   album, albumCategory,
@@ -143,12 +130,8 @@ module.exports = {
   bind, bindDetail,
   // 会员
   member, memberDetail, memberRank,
-  // 分销
-  dMember, dData, dBouns,
   // 订单
   orderRefund, orderRefundDetail, orderReview, order, orderListDetail, orderDetail, orderExpress, orderPrice, orderUpdate,
-  // 商城数据
-  data, purchase,
   // 服务缴费
   receiptList, receiptLog, journal,
   //服务进度
@@ -156,9 +139,5 @@ module.exports = {
   // 积分
   point, pointProduct, pointRule, pointList, pointRank, signIn,
   // 推广
-  spread, spreadRank, posters,
-  // 微传单
-  wcd, wcdCase,
-  // 微网站
-  mobile, mobileCase
+  spread, spreadRank, posters
 }
